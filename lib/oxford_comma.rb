c6a290a6e6a38bf05f8187fd8be89e9c99327335
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 20:00:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  if array.length == 1
+  if array.length === 1
     array.join
-  elsif array.length == 2
+  elsif array.length === 2
     
-  elsif array.length == 3
+  elsif array.length === 3
     
   else
     
