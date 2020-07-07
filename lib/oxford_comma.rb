04c7e5df984bee3382acba413e6a11bc6697f381
Ben Iburg <ben.iburg@gmail.com>
2020-07-07 20:10:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ def oxford_comma(array)
   elsif array.length === 2
     array.join(" and ")
   elsif array.length === 3
-    array.join(", ")
-    array[-1].join(", and")
+    "#{array[0...-1].join(', ')} and #{array.last}"
   else
     
   end
